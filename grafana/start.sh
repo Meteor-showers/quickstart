7b6bdc1f889b1c9647a9513bb101b1350afcc4b6
zhanglikun <x@zahui.fan>
2024-12-31 23:54:14 +0800

    初始化仓库

---
+++ grafana/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd $(dirname $0)
+
+../public/docker-network.sh
+
+docker run -d --name=grafana \
+    --network iuxt \
+    --restart always \
+    --user "$(id -u)" \
+    --volume "$PWD/data:/var/lib/grafana" \
+    -e "GF_SERVER_ROOT_URL=https://grafana.babudiu.com/" \
+    -e "GF_INSTALL_PLUGINS=grafana-clock-panel" \
+    --add-host=host.docker.internal:host-gateway \
+    grafana/grafana
+
+
+../public/add_config_to_nginx.sh
